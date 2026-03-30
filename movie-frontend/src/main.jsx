a4e8eb9f3251767f580ff5415695d0f7ac31c487
prince <a0909070650@gmail.com>
2026-03-31 00:33:07 +0800

    Fix React App import and update Tailwind configuration

--- movie-frontend/src/main.jsx
+++ movie-frontend/src/main.jsx
@@ -1,9 +1,13 @@
-// 修改前
-import { BrowserRouter } from 'react-router-dom'
-// 修改後
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // <--- 就是這行不見了！
 import { HashRouter } from 'react-router-dom'
+import './index.css' // 你的全域樣式
 
-// 下方的標籤也要改
-<HashRouter>
-  <App />
-</HashRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+)
