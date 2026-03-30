e3177d8b189da1ac65aad9494835b10d0ce935d0
prince <a0909070650@gmail.com>
2026-03-31 00:41:24 +0800

    調整CSS全部遺失

--- movie-frontend/vite.config.js
+++ movie-frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vienna-cinema_online/', // 這裡一定要對應你的 GitHub 儲存庫名稱
+  // 修改這行：這是一個神奇的設定，它會讓你的 public 資料夾裡的所有圖片，
+  // 無論在本機還是 GitHub 上，前面都自動加上正確的專案名稱
+  base: '/vienna-cinema_online/', 
 })
